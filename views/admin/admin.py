6e980655c725e6c3c2727c45e4ff1b301688c45e
Grandkojo <essienernest.kojoowus@gmail.com>
2024-06-10 19:19:53 +0000

    completed initial setup

--- views/admin/admin.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask, url_for, request, redirect, render_template, Blueprint
-from flask_sqlalchemy import SQLAlchemy
-from dotenv import load_dotenv
-import os
-
-admin_bp = Blueprint("admin", __name__, template_folder="templates")
