0ddc1e7b7768edeb80cec91d693f9930c5633cd5
Grankojo <essienernest.kojoowusu@gmail.com>
2024-08-10 01:01:25 +0000

    tweaked some of the css to fit the mobile view

--- app.py
+++ app.py
@@ -37,4 +37,5 @@ if __name__ == '__main__':
     from views.db.db import *
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0')
+    # app.run(host='0.0.0.0')#for demo
+    app.run(host='0.0.0.0', port=5300) #for other users(refer to get_location.html line 90)
